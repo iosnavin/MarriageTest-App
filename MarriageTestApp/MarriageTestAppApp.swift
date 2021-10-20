d3cba850856e105bf913984131b154fe693fa3c6
Naveen <ios.nreddy@gmail.com>
2021-10-20 15:24:55 +0530

    Initial Commit

---
+++ MarriageTestApp/MarriageTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarriageTestAppApp.swift
+//  MarriageTestApp
+//
+//  Created by Naveen Reddy on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MarriageTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
